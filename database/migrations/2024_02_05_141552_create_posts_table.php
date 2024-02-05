e20fc7b0dc9423d39bc8a3f9b8e5c1aa82a00dda
David <dterenceabanulo01@gmail.com>
2024-02-05 14:46:50 +0000

    created seeds for comments and posts and added foreign key relationship for user to posts

--- database/migrations/2024_02_05_141552_create_posts_table.php
+++ database/migrations/2024_02_05_141552_create_posts_table.php
@@ -16,9 +16,10 @@ return new class extends Migration
             $table->string('content');
             $table->string('title');
             $table->bigInteger('user_id')->unsigned();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+
             $table->timestamps();
-
-
         });
     }
 
