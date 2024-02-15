4e5c51324d63cd85958ed9d05fcecb54acb46608
David <dterenceabanulo01@gmail.com>
2024-02-15 10:57:24 +0000

    added one to many relationships and factory for comments for users.

--- database/migrations/2024_02_05_141604_create_comments_table.php
+++ database/migrations/2024_02_05_141604_create_comments_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->string('content');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
--- database/seeders/CommentTableSeeder.php
+++ database/seeders/CommentTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comment;
 
 class CommentTableSeeder extends Seeder
 {
@@ -12,6 +13,11 @@ class CommentTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $a = new Comment;
+        $a-> content="This is my post";
+        $a->user_id =1;
+        $a->save();
+
+        Comment::factory()->count(10)->create();
     }
 }
