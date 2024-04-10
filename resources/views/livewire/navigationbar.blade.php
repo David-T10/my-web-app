43e3120bc6c546e5c644caaa008f5e335abd1f66
David <dterenceabanulo01@gmail.com>
2024-04-10 19:13:20 +0100

    guest navbar

--- resources/views/livewire/navigationbar.blade.php
+++ resources/views/livewire/navigationbar.blade.php
@@ -1,7 +1,9 @@
 <div class="container mx-auto mt-5">
     <div class="flex justify-center space-x-4">
         <a href="{{ route('posts.index') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg">View All Posts</a>
+        @if (Auth::check())
         <a href="{{ route('users.show', ['id' => Auth::id()]) }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-6 rounded-lg">View Profile</a>
+        @endif
         <a href="{{ route('posts.create') }}" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-3 px-6 rounded-lg">Create Post</a>
     </div>
 </div>
